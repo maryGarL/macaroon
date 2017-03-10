853bc09a785f18b6e56266de30edcf3c7c585115
Mariel Garcia <maryel_565@hotmail.com>
2017-03-10 10:39:39 -0500

    terminado index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,24 @@
-var alerta1= document.getElementById('alerta');
-var alerta2= document.getElementById('error');
-var admitir= ["maryel_565@hotmail.com","maryel@hotmail.com","laboratoria@laboratoria.la","gian@laboratoria.la", "papu@laboratoria.la"]
-var nombre= document.getElementById('usuario');
-var contrasenha= document.getElementById('contra');
-
+var user= document.getElementById('alerta'); //mensaje de error  en span si no cumple las condiciones de email
+var pass= document.getElementById('error');//mensaje de error  en span si no cumple las condiciones de password
+var arr= ["maryel_565@hotmail.com","maryel@hotmail.com","laboratoria@laboratoria.la","gian@laboratoria.la", "papu@laboratoria.la"]
+var nombre= document.getElementById('usuario'); //id del input email
+var contrasenha= document.getElementById('contra');//id del input password
+var confirmar = false;
 
 function validate(){
-  if (!(/\w{1,}[@][\w\-]{1,}([.]([\w\-]{1,})){1,3}$/.test(nombre.value))||(nombre.value.length==0)){
-    alerta.innerHTML="La dirección de email es incorrecta."
-    return false;
+  if (!(/\w{1,}[@][\w\-]{1,}([.]([\w\-]{1,})){1,3}$/.test(nombre.value))||(nombre.value.length==0)||(nombre.value==null)){
+    user.innerHTML="La dirección de email es incorrecta."
+  return confirmar;}
+  if((contrasenha.value.length==0)||(contrasenha.value==null)||(contrasenha.value.length<7)){
+    pass.innerHTML="Escribe un paswword"
+  return confirmar;}
 
-    }
-
-})
+  arr.forEach(function(elemento){
+    if(elemento==nombre.value){
+     confirmar = true;
+   }else{
+     user.innerHTML="La dirección de email es invalida"
+   }
+  });
+  return confirmar;
+  }
