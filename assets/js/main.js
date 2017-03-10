b40d54ffd1904c4b4b537280e4d0220222d63e6e
maryGarL <maryel_565@hotmail.com>
2017-03-10 08:51:58 -0500

    modificacion disque yo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,15 @@
-var alerta1= document.getElementById('valida1');
-var alerta2= document.getElementById('valida2');
+var alerta1= document.getElementById('alerta');
+var alerta2= document.getElementById('error');
 var admitir= ["maryel_565@hotmail.com","maryel@hotmail.com","laboratoria@laboratoria.la","gian@laboratoria.la", "papu@laboratoria.la"]
-var nombre= document.getElementById('persona');
+var nombre= document.getElementById('usuario');
 var contrasenha= document.getElementById('contra');
 
 
 function validate(){
   if (!(/\w{1,}[@][\w\-]{1,}([.]([\w\-]{1,})){1,3}$/.test(nombre.value))||(nombre.value.length==0)){
-    alerta1.innerHTML="La dirección de email es incorrecta."
+    alerta.innerHTML="La dirección de email es incorrecta."
     return false;
 
-    for each (var i in admitir){
-      if (nombre.value!=admitir[i]) {
-        alerta1.innerHTML="La dirección de email es incorrecta."
-      };
-      return false;
     }
 
-    }
-  }
-}
+})
